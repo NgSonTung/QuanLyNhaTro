d9fb4917060e6ca8a05c01a8482adf7b9908ba74
NgSonTung <ngsondecon@gmail.com>
2022-04-04 07:45:46 +0700

    link form

--- QuanLyNhaTro/Program.cs
+++ QuanLyNhaTro/Program.cs
@@ -16,8 +16,7 @@ namespace QuanLyNhaTro
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new Form1());
-            Application.Run(new Admin());
+            Application.Run(new Form1());
         }
     }
 }
